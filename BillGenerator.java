b6dc2b63d4174c406bbe7b6f6bacdd82d73526ff
bhavyachanan <bhavyachanan@gmail.com>
2019-09-20 15:07:08 +0530

    BillGenerator.java
    This is the loader class

--- BillGenerator.java
+++ BillGenerator.java
@@ -1,18 +1,23 @@
-class BillGenerator
+class BillGenerator implements PriceCalculator
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		
-		
-		Category c = new Category("Electronics" ,  new Slab(18));
-Product p = new Product("Mobile",2,5000,c);
-Product p1 =new Product ("TV", 5, 6000, c);
 
-System.out.println(Cart.priceCalculator(p.getQuantity(),p.getPrice(),p.getSlab()));
-//assertValue(11800,Cart.priceCalculator(p.getQuantity(),p.getPrice(),p.getSlab())) =>for testing
-System.out.println(Cart.priceCalculator(p1.getQuantity(),p1.getPrice(),p1.getSlab()));
-System.out.println(Cart.totalBill);
-System.out.println(p.getQuantity()*(p.getPrice()+(p.getPrice() * p.getSlab()/100)));
+	int calculator(Product product)
+	{
+		return product.getQuantity()*(product.getPrice()+(product.getPrice() * product.getSlab()/100))
+	}
+
+	public static void main (String[] args)
+	{
+		
+		
+		
+		Category category = new Category("Food" ,  new Slab(18));
+		Product rice = new Product("Rice",2,5000,category);
+		Product wheat =new Product ("Wheat", 5, 6000, category);
+
+		BillGenerator bill = new BillGenerator();
+		System.out.println(bill.calculate(rice));
+		AssertVale(11800,bill.calculate(rice)); // for testing
+
 	}
 }
